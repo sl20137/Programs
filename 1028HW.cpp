48c0fd6885ed77574e6c9e227b08cd6fe262e219
sl20137 <78151294+sl20137@users.noreply.github.com>
2022-05-26 21:15:54 +0800

    Symbol Fun

---
+++ 1028HW.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	string symbol;
+	cin>>symbol;
+	cout<<"  "<<symbol<<"  "<<endl;
+	cout<<" "<<symbol<<symbol<<symbol<<" "<<endl;
+	cout<<symbol<<symbol<<symbol<<symbol<<symbol<<endl;
+	cout<<" "<<symbol<<symbol<<symbol<<" "<<endl;
+	cout<<"  "<<symbol<<"  ";
+	return 0;
+}
